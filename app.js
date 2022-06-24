052e1fd1a841dcc8e0aa0544f6ecacf7bd13051a
anhviptn <47285465+anhviptn@users.noreply.github.com>
2022-06-24 20:06:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
-var createError = require('http-errors');
+const cron = require('node-cron');
+const axios = require('axios');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// ...
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Schedule tasks to be run on the server.
+var url = request.query.url;
+cron.schedule('*/1 * * * * *', function() {
+    axios
+        .get(url)
+        .then(res => {
+            console.log(`URL: ${url} => statusCode: ${res.status}`);
+            //console.log(res);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
